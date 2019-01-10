93f83b9547325929c20297a8581ff0f1eadafdc1
Franklin P. Thaker <franklin873@live.com>
2019-01-10 16:29:44 +0530

    Add files via upload

---
+++ FIND.js
@@ -0,0 +1,22 @@
+var mongo = require('mongodb').MongoClient
+var age = process.argv[2]
+
+var url = 'mongodb://localhost:27017/learnyoumongo'
+
+mongo.connect(url, function(err, db) {
+  if (err) throw err
+  let dbo = db.db("learnyoumongo")
+  let parrots = dbo.collection('parrots');
+  parrots.find({
+    age: {
+      $gt: +age
+    } 
+  },
+  {fields:{_id: 0}}
+  ).toArray(function(err, docs) {
+    if (err) throw err    
+    //console.log(docs);
+        console.log(docs);  // { field1: '...', field2: '...' }
+    });
+    db.close()
+  })
